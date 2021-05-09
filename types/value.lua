46d500f1778f8a05a4494e0256d2c0c03e69818b
Johan-Mi <johanmi@protonmail.com>
2021-05-09 16:01:58 +0200

    Make `Value._value` a private member

--- types/value.lua
+++ types/value.lua
@@ -5,22 +5,22 @@ require 'result'
 require 'error'
 
 function Value:__tostring()
-    return tostring(self.inner)
+    return tostring(self._value)
 end
 
 function Value.new(inner)
-    obj = { value = inner }
+    obj = { _value = inner }
     setmetatable(obj, Value)
     return obj
 end
 
 function Value:is_cons()
-    return getmetatable(self.value) == Cons
+    return getmetatable(self._value) == Cons
 end
 
 function Value:as_cons()
     if self:is_cons() then
-        return Some(self.value)
+        return Some(self._value)
     else
         return None
     end
